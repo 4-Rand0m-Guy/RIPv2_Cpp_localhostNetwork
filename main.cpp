448a9535ee2c38a7e1c4bd2b0997b87343b663cc
plr37 <plr37@uclive.ac.nz>
2018-04-27 23:20:20 +1200

    git chore

--- main.cpp
+++ main.cpp
@@ -18,10 +18,10 @@ int main(int argc, char **argv) {
     ConfigImporter configImporter = ConfigImporter(filename);
     Configuration cfg = configImporter.get_configuration();
     Rip daemon = Rip(cfg.routerID, cfg.input_ports, cfg.outputs, cfg.timer);
-    daemon.run();
+//    daemon.run();
     std::cout << "Crashing..." << std::endl;
 
-   /* unsigned char arr[24] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+   unsigned char arr[24] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                              '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                              '0', '1', '2', '3'};
 
@@ -32,12 +32,12 @@ int main(int argc, char **argv) {
     RIPRouteEntry rte = RIPRouteEntry(9, 4, 8);
     unsigned char ra[20];
     rte.serialize(ra);
-//    packet.addRoute(ra);
+    packet.addRoute(ra);
 
 //    std::cout << rte.toString() << std::endl;
-
-    std::string outp = packet.toString();
-    std::cout << "the packet: " << std::endl;
-//     std::cout << hdr.toString() << std::endl;*/
+//    std::string outp = packet.toString();
+//    std::cout << "the packet: " << std::endl;
+//     std::cout << hdr.toString() << std::endl;
+    daemon.processResponse(ha);
     return 0;
 }
